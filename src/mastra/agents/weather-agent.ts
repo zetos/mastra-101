632dc921d5ec357a2724a3ff022bfb8c303d01b8
Zetos Lima <cesar_manoel@hotmail.com>
2025-11-25 19:16:30 -0300

    chore: ch weather db name

--- src/mastra/agents/weather-agent.ts
+++ src/mastra/agents/weather-agent.ts
@@ -47,7 +47,7 @@ export const weatherAgent = new Agent({
   },
   memory: new Memory({
     storage: new LibSQLStore({
-      url: 'file:../mastra.db', // path is relative to the .mastra/output directory
+      url: 'file:../weather-mastra.db', // path is relative to the .mastra/output directory
     }),
   }),
 });
